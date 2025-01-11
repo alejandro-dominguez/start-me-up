f3eec372062de383f2991aaad0c87920c677394f
Alejandro Dominguez <alejandro.dominguez1@live.com>
2025-01-11 10:17:23 -0300

    updated SearchbarQueryText styles

--- components/homeComponents/SearchbarQueryText.tsx
+++ components/homeComponents/SearchbarQueryText.tsx
@@ -1,6 +1,6 @@
 const SearchbarQueryText = ({ searchBarQuery }: { searchBarQuery?: string | '' }) => {
     return (
-        <p className='font-semibold text-[1.35rem] mt-[.85rem] md:mt-[1.72rem] mb-3 md:mb-0
+        <p className='font-semibold text-[1.35rem] mt-4 md:mt-[1.65rem] mb-3 md:mb-[1.32rem]
         flex flex-col md:flex-row items-center justify-between break-all'>
             <span className='flex flex-col md:flex-row xs:gap-1 mx-auto md:ml-0
             xs:items-baseline self-start leading-7 text-center'>
@@ -8,7 +8,7 @@ const SearchbarQueryText = ({ searchBarQuery }: { searchBarQuery?: string | '' }
                     Resultados para:
                 </span>
                 <span className='text-lg tracking-wide text-primary-200 mx-auto
-                text-center xs:-mt-1 md:-mt-0 mb-2'>
+                text-center xs:-mt-1'>
                     {` "${searchBarQuery}"`}
                 </span>
             </span>
