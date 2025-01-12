d668846ecde2b0a8cef9418c28c791633ae5a04e
Alejandro Dominguez <alejandro.dominguez1@live.com>
2025-01-11 22:24:02 -0300

    updated UserPageSkeleton margins

--- components/userComponents/UserPageSkeleton.tsx
+++ components/userComponents/UserPageSkeleton.tsx
@@ -7,7 +7,7 @@ const UserPageSkeleton = () => {
             <div className='flex flex-col justify-center mt-[3.75rem] xs:mt-[4.2rem] md:mt-[4.5rem]
             md:ml-0 md:mr-7 lg:mr-9 min-w-72 sm:min-w-[28rem] items-center bg-zinc-400 animate-pulse
             mx-auto border-[5px] border-black shadow-100 h-[15rem] rounded-[30px] relative z-0' />
-            <ul className='grid grid-cols-1 lg:grid-cols-2 gap-7 pb-10'>
+            <ul className='grid grid-cols-1 lg:grid-cols-2 gap-7 pb-10 mt-5 md:mt-0'>
                 {
                     [0, 1, 2, 3, 4, 5].map((i: number) => (
                         <li key={cn('skeleton', i)}>
