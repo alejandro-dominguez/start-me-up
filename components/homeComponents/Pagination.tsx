a4bd079adc8cbf382fd76dfb70fa1fcbdc2d93f7
Alejandro Dominguez <alejandro.dominguez1@live.com>
2025-01-16 20:57:27 -0300

    updated Pagination styles

--- components/homeComponents/Pagination.tsx
+++ components/homeComponents/Pagination.tsx
@@ -49,8 +49,8 @@ const Pagination = ({
                     type='button'
                     onClick={() => switchPages('prevPage')}
                     className='px-4 xs:px-5 py-[.4rem] bg-slate-300/80 rounded-full text-black/90
-                    shadow-sm shadow-black/15 transition-colors hover:bg-slate-300
-                    ease-out focus-within:bg-slate-300 disabled:bg-slate-300/45
+                    shadow-sm shadow-black/15 transition-colors ease-out hover:bg-slate-400
+                    focus-within:bg-slate-400 disabled:bg-slate-300/45
                     disabled:shadow-black/10 disabled:text-black/60'
                     disabled={prevPageCondition}
                 >
